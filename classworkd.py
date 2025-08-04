782e8631fbd5d571323e04eb89a710d627cf0c78
Retostephen <retostephen2006@gmail.com>
2025-08-04 15:18:17 +0100

    Trying to solve on Task 2

--- classworkd.py
+++ classworkd.py
@@ -1,3 +1,6 @@
+'''
+#Task 1
+
 score = int(input("Enter the score of the student: "))
 
 if score >= 70 and score <= 100:
@@ -12,3 +15,78 @@ elif score >= 0 and score <= 30:
     print("The grade is F")
 else:
     print("This is an invalid range")
+
+'''
+
+'''
+Task 2: Smart Restaurant Discount System
+
+You are helping a restaurant in Jos implement an automated discount system for their weekend promo.
+
+The rules are:
+1. Customers with a loyalty card get a 10% discount.
+2. If the customer's order amount is above 20,000 NGN:
+    - Loyalty card holders get an additional 5% discount.
+    - Non-loyalty card holders get a free soft drink instead.
+3. Students (verified with student ID) get an extra 5% discount on top of whatever they qualify for.
+
+Customer data is stored in a dictionary like this:
+
+customer = {
+    "name": "Godiya",
+    "order_amount": 25000,
+    "loyalty_card": True,
+    "is_student": False
+}
+
+Write a nested if-else block that:
+1. Calculates the final discount or freebie for the customer.
+2. Stores the result in a dictionary called `order_summary`.
+3. Prints out a summary for the cashier.
+'''
+
+customer = {
+"name": "Godiya",
+"order_amount": 25000,
+"loyalty_card": True,
+"is_student": False,
+}
+
+order_summary = {}
+discount_percentage = 0
+freebie = None
+
+if customer["loyalty_card"]:
+    discount_percentage += 10
+    if customer["order_amount"] > 20000:
+        discount_percentage += 5
+    else:
+        freebie = "Free soft drink"
+
+if customer["is_student"]:
+    discount_percentage +=5
+
+
+final_discount_amount = customer["order_amount"] * (discount_percentage / 100)
+final_amount_after_discount = customer["order_amount"] - final_discount_amount
+
+order_summary = {
+"customer_name": customer["name"],
+"original_amount": customer["order_amount"],
+"discount_percentage": discount_percentage,
+"final_discount_amount": final_discount_amount,
+"final_amount_after_discount": final_amount_after_discount,
+"freebie": freebie
+}
+
+print(f"Order Summary for {order_summary['customer_name']}")
+print(f"Original Order Amount: {order_summary['original_amount']}")
+print(f"Discount Applied: {order_summary['discount_percentage']}% ({order_summary['final_discount_amount']:.2f})")
+print(f"Final Amount: {order_summary['final_amount_after_discount']:.2f}")
+if order_summary["freebie"]:
+    print(f"Freebie: {order_summary['freebie']}")
+
+
+
+
+
