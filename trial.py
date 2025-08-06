16fdc6a1cd8ffd2fad0c2806e8f5eaf5fcf84f3b
Retostephen <retostephen2006@gmail.com>
2025-08-06 13:36:12 +0100

    Still on tasks

--- trial.py
+++ trial.py
@@ -29,8 +29,41 @@ keys_list = {*countries}
 print(keys_list)
 print(type(keys_list))
 '''
-
+'''
 a = {"name": 1, "name2": 2}
-b = {"class": 3, "class": 4}
+b = {"class1": 3, "class": 4}
 c = {"class": b}
+a.update(b)
+print(a)
 print(c)
+'''
+'''
+tanko = {"name": "tanko"}
+core = {"name": "core"}
+
+tanko.update(core)
+#core.update(tanko)
+
+print(tanko)
+'''
+
+password = input("Enter your password: ")
+confirm_password = input("Please confirm your password: ")
+
+if password == confirm_password:
+	print("Accepted")
+else:
+	print("Passwords Doesn't Match")
+
+
+
+
+
+
+
+
+
+
+
+
+
