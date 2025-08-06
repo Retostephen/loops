43f6b0ced2bc34f25c733187f562f39c1c4efb42
Retostephen <retostephen2006@gmail.com>
2025-08-06 12:57:49 +0100

    Flexing around dictionaries

--- trial.py
+++ trial.py
@@ -8,21 +8,29 @@ print(name2)
 add = -3 // 2
 print(add)
 '''
+
+
+'''
 countries = {
 "Nigeria": "Abuja",
 "Ghana": "Accra",
 "Singapore": "Singapore"
 	}
 
-'''
+
 countries.update({"USA": "DC"})
 print(countries)
 countries = str(countries)
 keys_list = list(countries.keys(countries))
 print(keys_list)
 print(type(countries))
-'''
 
 keys_list = {*countries}
 print(keys_list)
 print(type(keys_list))
+'''
+
+a = {"name": 1, "name2": 2}
+b = {"class": 3, "class": 4}
+c = {"class": b}
+print(c)
