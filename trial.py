5ef188a5e7819de27009f4fa7f3c16e86d056a64
Retostephen <retostephen2006@gmail.com>
2025-08-06 12:41:07 +0100

    Still performing task.py on the revision

--- trial.py
+++ trial.py
@@ -1,5 +1,28 @@
+'''
 name = "Bankat"
 name2 = "Hello" + " " + name
 
 print(name)
 print(name2)
+
+add = -3 // 2
+print(add)
+'''
+countries = {
+"Nigeria": "Abuja",
+"Ghana": "Accra",
+"Singapore": "Singapore"
+	}
+
+'''
+countries.update({"USA": "DC"})
+print(countries)
+countries = str(countries)
+keys_list = list(countries.keys(countries))
+print(keys_list)
+print(type(countries))
+'''
+
+keys_list = {*countries}
+print(keys_list)
+print(type(keys_list))
